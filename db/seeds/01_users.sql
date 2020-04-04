9e6349e2ece07303515b6b7da7773272f44eef75
BilliMorgan <shimko20@gmail.com>
2020-04-04 20:48:13 +0000

    add seesd code to insert fake values into each table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,12 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password, profile_pic_url)
+VALUES ('Alice', 'alice@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'http://mypicture.jpeg'),
+('Veronica', 'veronica@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'http://mypicture.jpeg'),
+('Michale', 'michale@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'http://mypicture.jpeg'
+);
+
+INSERT INTO categories (category)
+VALUES ('language');
+
+
+
---
+++ db/seeds/06_comments.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO comments (user_id, resource_id, content)
+VALUES (1, 2, 'Loved it, highly recomended');
