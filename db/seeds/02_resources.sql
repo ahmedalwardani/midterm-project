33a3f5e20b6c4254fc21edb90920ac7ced4dbe66
BilliMorgan <shimko20@gmail.com>
2020-04-05 01:15:44 +0000

    added .env file

--- db/seeds/02_resources.sql
+++ db/seeds/02_resources.sql
@@ -1,4 +1,6 @@
 
 INSERT INTO resources (owner_id, title, type, url, description)
-VALUES (1, 'My first resource here', 'article', 'https://node-postgres.com/guides/project-structure', 'this is article about sugested project structure'
+  VALUES (1, 'My first resource here', 'article', 'https://node-postgres.com/guides/project-structure', 'this is article about sugested project structure'
+  ),
+  (2, 'Its second resource here', 'video', 'https://node-postgres.com/guides/project-structure', 'this is a video about sugested project structure'
 );
