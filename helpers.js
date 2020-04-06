554690a27b176f428c3ba2eaef7c7888411f7683
ahmedalwardani <aalwa049@uottawa.ca>
2020-04-06 22:50:29 +0000

    fixing routes-apr06

--- helpers.js
+++ helpers.js
@@ -1,25 +1,17 @@
 //Returns an object corresponding to a provided string, otherwise returns undefined
-
-  const queryFunction = (statement, values, callback) => {
-    return pool.query(statement, values)
-      .then(res => callback(res.rows));
-  }
-
-
-const getUserByEmail = (email, db) => {
-  return db
-  .query(`
+const getUserByEmail = (db, email) => {
+  return db.query(`
     SELECT *
     FROM users
     WHERE email = $1
   `, [email])
     .then(res => {
-      return res.rows[0]
+      return res.rows[0];
     })
     .catch(err => console.error('query error', err.stack));
 };
 
-const addUser = function (user, db) {
+const addUser = function(user, db) {
   let arr = ['bob', user.email, user.password];   //change bob to user.name
   return db
     .query(`
@@ -27,115 +19,60 @@ const addUser = function (user, db) {
       VALUES ($1, $2, $3) RETURNING *;
   `, arr)
     .then(res => {
-      console.log(res.rows[0])
-      return res.rows[0]
+      return res.rows[0];
     })
     .catch(err => console.error('query error', err.stack));
-}
-const addResources = function(resource) {
-  let resourceValues = Object.values(resource)
+};
+
+const addResource = function(resource, db) {
+  let resourceValues = Object.values(resource);
   return db
-  .query(
-    `INSERT INTO resources (
+    .query(
+      `INSERT INTO resources (
     owner_id, title, type, url, description)
     VALUES($1, $2, $3, $4, $5) RETURNING *;
   `, resourceValues)
-  .then(res => {
-    return res.rows[0]
-  })
-  .catch(err => console.error('query error', err.stack));
+    .then(res => {
+      return res.rows[0];
+    })
+    .catch(err => console.error('query error', err.stack));
 };
 
 
 //function resourcesForUser has not connected yet
-const resourcesForUser = function (id, db){
+const resourcesForUser = function(id, db) {
   return db
-  .query(
-    `SELECT resources.*
+    .query(
+      `SELECT resources.*
     FROM resources
     JOIN users ON owner_id = users.id
     WHERE users.id = $1;
     `, [id])
     .then(res => {
-      return res.rows[0]
+      console.log(res.rows[0]);
     })
     .catch(err => console.error('query error', err.stack));
-  };
+};
 
-const singleResource = function(id, db) {
-  return db.queryFunction(
+const singleResource = function(db, id) {
+  return db.query(
     `
-    SELECT* FROM resources
-    WHERE resources.id = $1;
-    `
-  , [id], rows => console.log(rows[0]));
-};
-
-
-// const resourcesForUser = (id, resourcesDatabase) => {
-
-//   const resourcesObject = {};
-//   for (const key in resourcesDatabase) {
-//     if (resourcesDatabase[key].userID === id) {
-//       resourcesObject[key] = resourcesDatabase[key];
-//     }
-//   }
-//   return resourcesObject;
-// };
-
-
-const resourcesDatabase = {
-  b2xVn2: {title: "test resource 1", resourceURL:"http://www.lighthouselabs.ca", userID: "test 1", thumbnail: "thumbnail 1", owner: "owner 1", isSaved: true, averageRating: 5, ratings: [{rating: 5, comment: "awesome"}, {rating: 5, comment: "cool"}, {rating: 5, comment: "original"},]},
-
-  qsm5xK: {title: "test resource 2", resourceURL:"http://www.google.ca", userID: "test 2", thumbnail: "thumbnail 2", owner: "owner 2", isSaved: false, averageRating: 3, ratings: [{rating: 3, comment: "not bad"}, {rating: 3, comment: "bad"}, {rating: 3, comment: "average"},]},
+    SELECT * FROM resources
+    WHERE resources.owner_id = $1;
+    `, [Number(id)])
+    .then(resp => resp.rows[0])
+    .catch(err => console.error('query error', err.stack));
 };
 
 
 
 
-
-
-const generateRandomString = () => {
-  const str = "abcdefghijklmnopqrstuvwxyz0123456789";
-  let ans = "";
-  let i = 0;
-  while (i < 6) {
-    ans += str[Math.floor(Math.random() * str.length)];
-    i++;
-  }
-  return ans;
-};
-
-const users = {
-  "Ahmed": {
-    name: "Ahmed",
-    id: "1",
-    email: "ahmed@ahmed.com",
-    password: "password"
-  },
-  "Linh": {
-    name: "Linh",
-    id: "2",
-    email: "linh@linh.com",
-    password: "password"
-  },
-  "Andrii": {
-    name: "Andrii",
-    id: "3",
-    email: "andrii@andrii.com",
-    password: "password"
-  },
-};
 
 module.exports = {
   addUser,
-  addResources,
+  addResource,
   getUserByEmail,
-  users,
   resourcesForUser,
-  resourcesDatabase,
-  generateRandomString,
   singleResource,
-  queryFunction
 };
 
--- routes/description.js
+++ routes/description.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const router  = express.Router();
-const {users, resourcesForUser, resourcesDatabase, singleResource} = require("../helpers");
+const {singleResource} = require("../helpers");
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+    const ratingsArray = [];
     const currentUser = req.session.user_id;
-    const resourceObject = singleResource(currentUser, resourcesDatabase);
-    const userObject = users[currentUser];
-    const templateVars = {resource: resourcesObject, user: userObject};
-    res.render("description", templateVars);
+    singleResource(db, currentUser).then((resp) => {
+      const resource = {
+        id: resp.id,
+        owner_id: resp.owner_id,
+        title: resp.title,
+        type: resp.type,
+        url: resp.url,
+        thumbnail: null,
+        description: resp.description,
+        active: resp.active,
+        //saved: use query to see whether user saved resource in the saved_resources table
+        owner: currentUser === resp.owner_id ? true : false,
+        ratings: ratingsArray
+      };
+
+      res.render("description", {resource});
+
+    }).catch(err => console.log(err));
+
   });
   return router;
 };
-
