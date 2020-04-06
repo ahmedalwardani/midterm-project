9a3f194ca0007a957dcf735f537ff34f4e1b4f4f
Linh095 <linh.nguyen.nvhl@gmail.com>
2020-04-06 15:23:42 +0000

    renamed files to match routes

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get("/", (req, res) => {
   if (currentUser) {
     res.render("index", templateVars);
   } else {
-    res.render("index", {user: null});
+    res.render("landing_page", {user: null});
   }
 });
 
