aa49048c69eda70d6e1685a8280e65a3f368fee2
BilliMorgan <shimko20@gmail.com>
2020-04-09 15:01:48 +0000

    new branch

---
+++ public/scripts/addnewtopic.js
@@ -0,0 +1,26 @@
+// $(() => {
+//   $.ajax({
+//     method: "get",
+//     url: "/new-topic"
+//   }).done(function(){
+//     //alert( "$.get succeeded" )
+//     console.log("Hello!!")
+//     // for (user of users) {
+//     //   $("<div>").text(user.name).appendTo($("body"));
+//     // }
+//   });;
+// });
+// const express = require('express');
+// const router  = express.Router();
+
+module.exports = (db) => {
+router.post("/", (req, res)=>{
+  const data = req.body;
+  console.log(data)
+
+  
+})
+return router;
+};
+
+
